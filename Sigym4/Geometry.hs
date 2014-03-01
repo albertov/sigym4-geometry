db3c8495fb86012011f6c83aabd1f64443b0d6ee
Alberto Valverde <alberto@meteogrid.com>
2014-03-01 08:48:58 +0100

    Size is now a wrapper around a (v Int)

--- Sigym4/Geometry.hs
+++ Sigym4/Geometry.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE StandaloneDeriving
+{-# LANGUAGE GeneralizedNewtypeDeriving
+           , StandaloneDeriving
            , FlexibleContexts
            , FlexibleInstances
            , MultiParamTypeClasses
@@ -10,22 +11,27 @@
            #-}
 module Sigym4.Geometry (
     Geometry (..)
-  , GeometryType (..)
+  , Point
   , Feature (..)
   , IsVertex
-  , Vertex
+  , HasOffset
   , Pixel (..)
   , Size (..)
+  , Offset (..)
+  , RowMajor
+  , ColumnMajor
   , Extent (..)
   , SpatialReference (..)
+  , GeoTransform (..)
   , GeoReference
   , mkGeoReference
   , vertexOffset
-  , scalarSize
   , grScalarSize
   , grSize
-  , forward
-  , backward
+  , grTransform
+  , grSrs
+  , grForward
+  , grBackward
   , module V2
   , module V3
 ) where
@@ -36,6 +42,7 @@ import Control.Lens
 import Linear.Trace (Trace)
 import Linear.Vector (Additive)
 import Data.Foldable (Foldable)
+import Data.Maybe (fromMaybe)
 import Linear.V2 as V2
 import Linear.V3 as V3
 import Linear.Matrix ((!*), (*!), eye2, eye3, inv22, inv33)
@@ -43,85 +50,105 @@ import Linear.Epsilon (Epsilon)
 import Data.Typeable
 import Data.Foldable (product)
 
-type Coord = Double
-type Vertex v = v Coord
 -- | A squared Matrix
-type SqMatrix v = v (Vertex v)
+type SqMatrix v a = v (v a)
 
 -- | class for vertex types, used to simplify type signatures
-class ( Num (Vertex v), Eq (Vertex v), Show (Vertex v), Fractional (Vertex v)
-      , Num (SqMatrix v), Show (SqMatrix v), Eq (SqMatrix v)
+class ( Num a, Eq a, Show a, Epsilon a, Floating a
+      , Num (v a), Eq (v a), Show (v a)
+      , Num (SqMatrix v a), Show (SqMatrix v a), Eq (SqMatrix v a)
       ,Applicative v, Additive v, Foldable v, Trace v)
-  => IsVertex v
+  => IsVertex v a
   where
-    inv       :: SqMatrix v -> Maybe (SqMatrix v)
-    eye       :: SqMatrix v
-    linOffset :: Size v -> Pixel v -> Int
+    inv :: SqMatrix v a -> Maybe (SqMatrix v a)
+    eye :: SqMatrix v a
 
-instance IsVertex V2 where
+newtype Offset (t :: OffsetType) = Offset {unOff :: Int}
+  deriving (Eq, Show, Ord, Num)
+
+data OffsetType = RowMajor | ColumnMajor
+
+type RowMajor = 'RowMajor
+type ColumnMajor = 'ColumnMajor
+
+class HasOffset v (t :: OffsetType) where
+    toOffset :: Size v -> Pixel v -> Maybe (Offset t)
+
+instance (Num a, Eq a, Show a, Epsilon a, Floating a) => IsVertex V2 a where
     inv = inv22
     eye = eye2
-    linOffset s p = p'^._y * s'^._x
-                  + p'^._x
-      where p' = fmap truncate $ unPx p
-            s' = fmap ceiling  $ unSize s
 
-instance IsVertex V3 where
+instance HasOffset V2 RowMajor where
+    toOffset s p
+      | between (pure 0) s' p' = Just (Offset o)
+      | otherwise              = Nothing
+      where o  = p'^._y * s'^._x
+               + p'^._x
+            s' = unSize s
+            p' = fmap floor $ unPx p
+    {-# INLINE toOffset #-}
+
+between :: (Applicative v, Ord a, Num (v a), Num a, Eq (v Bool))
+  => v a -> v a -> v a -> Bool
+between lo hi v = (fmap (>  0) (hi - v) == pure True) &&
+                  (fmap (>= 0) (v - lo) == pure True)
+
+instance (Num a, Eq a, Show a, Epsilon a, Floating a) => IsVertex V3 a where
     inv = inv33
     eye = eye3
-    linOffset s p = p'^._z * (s'^._x * s'^._y)
-                  + p'^._y * s'^._x
-                  + p'^._x
-      where p' = fmap truncate $ unPx p
-            s' = fmap ceiling  $ unSize s
+
+instance HasOffset V3 RowMajor where
+    toOffset s p
+      | between (pure 0) s' p' = Just (Offset o)
+      | otherwise              = Nothing
+      where o  = p'^._z * (s'^._x * s'^._y)
+               + p'^._y * s'^._x
+               + p'^._x
+            s' = unSize s
+            p' = fmap floor $ unPx p
+    {-# INLINE toOffset #-}
 
 
 -- | An enumeration of geometry types
 data GeometryType = Point
     deriving (Show, Eq, Enum)
 
-deriving instance Typeable 'Point
+type Point = 'Point
+deriving instance Typeable Point
 
 -- | A GADT used to represent different geometry types, each constructor returns
 --   a geometry type indexed by 'GeometryType'
 data Geometry (t :: GeometryType) v where
-    MkPoint :: forall v. IsVertex v =>
-      {pVertex :: !(Vertex v)} -> Geometry Point v
+    MkPoint :: forall v. IsVertex v Double =>
+      {pVertex :: !(v Double)} -> Geometry Point v
 deriving instance Eq (Geometry t v)
 deriving instance Show (Geometry t v)
 deriving instance Typeable Geometry
 
 -- | An extent in v space is a pair of minimum and maximum vertices
 data Extent v where
-    Extent :: forall v. IsVertex v =>
-      {eMin :: !(Vertex v), eMax :: !(Vertex v)} -> Extent v
+    Extent :: forall v. IsVertex v Double =>
+      {eMin :: !(v Double), eMax :: !(v Double)} -> Extent v
 deriving instance Eq (Extent v)
 deriving instance Show (Extent v)
 deriving instance Typeable Extent
 
--- | A pixel is a pseudo-newtype around a vertex
-data Pixel v where
-    Pixel :: forall v. IsVertex v =>
-      {unPx :: !(Vertex v)} -> Pixel v
+-- | A pixel is a newtype around a vertex
+newtype Pixel v = Pixel {unPx :: v Double}
 
-deriving instance Eq (Pixel v)
-deriving instance Show (Pixel v)
-deriving instance Typeable Pixel
 
--- | A shape is a pseudo-newtype around a vertex, it represents an array of
---   dimensions (v1,..,vn)
+-- | A Size is a pseudo-newtype around a vector, it represents the dimensions
+--  of an array of shape (v1,..,vn)
 data Size v where
-    Size :: forall v. IsVertex v =>
-      {unSize :: !(Vertex v)} -> Size v
+    Size :: forall v. (Eq (v Int), Show (v Int)) =>
+      {unSize :: !(v Int)} -> Size v
 
 deriving instance Eq (Size v)
 deriving instance Show (Size v)
 deriving instance Typeable Size
 
-scalarSize :: IsVertex v => Size v -> Int
-scalarSize = product . fmap ceiling . unSize
-
--- integralSize = fmap ceiling . unSize
+scalarSize :: IsVertex v Double => Size v -> Int
+scalarSize = product . unSize
 
 -- | A feature of 'GeometryType' t, vertex type 'v' and associated data 'd'
 data Feature t v d = Feature {
@@ -139,9 +166,9 @@ data SpatialReference = SrsProj4 String
 -- 'Pixel' coordinates and back. gtMatrix *must* be inversible so smart
 -- constructors are provided
 data GeoTransform v  where
-    GeoTransform :: forall v. IsVertex v =>
-      { gtMatrix :: !(SqMatrix v)
-      , gtOrigin :: !(Vertex v)
+    GeoTransform :: forall v. IsVertex v Double =>
+      { gtMatrix :: !(SqMatrix v Double)
+      , gtOrigin :: !(v Double)
       } -> GeoTransform v
 deriving instance Eq (GeoTransform v)
 deriving instance Show (GeoTransform v)
@@ -150,33 +177,34 @@ deriving instance Typeable GeoTransform
 -- Makes a standard 'GeoTransform' for north-up images with no rotation
 -- northUpGeoTransform :: Extent V2 -> Pixel V2 -> Either String (GeoTransform V2)
 northUpGeoTransform ::
-  (IsVertex v, R2 v, Eq (v Bool)) =>
-  Extent v -> Size v -> Either String (GeoTransform v)
+  (IsVertex v Double, R2 v, Eq (v Bool), Fractional (v Double))
+  => Extent v -> Size v -> Either String (GeoTransform v)
 northUpGeoTransform e s
   | not isValidBox   = Left "northUpGeoTransform: invalid extent"
-  | not isValidSize = Left "northUpGeoTransform: invalid shape"
+  | not isValidSize  = Left "northUpGeoTransform: invalid size"
   | otherwise        = Right $ GeoTransform matrix origin
   where
     isValidBox  = fmap (> 0) (eMax e - eMin e)  == pure True
     isValidSize = fmap (> 0) s'                 == pure True
     origin      = (eMin e) & _y .~ ((eMax e)^._y)
-    s'          = unSize s
+    s'          = fmap fromIntegral $ unSize s
     dPx         = (eMax e - eMin e)/s' & _y %~ negate
     matrix      = pure dPx * eye
 
-forwardGT :: IsVertex v => GeoTransform v -> Vertex v -> Pixel v
-forwardGT gt v = Pixel $ m !* (v-v0)
-  where Just m = inv $ gtMatrix gt
-        v0     = gtOrigin gt
+gtForward :: IsVertex v Double => GeoTransform v -> v Double -> Pixel v
+gtForward gt v = Pixel $ m !* (v-v0)
+  where m   = fromMaybe (error "gtForward. non-inversible matrix")
+                        (inv $ gtMatrix gt)
+        v0  = gtOrigin gt
 
 
-backwardGT :: IsVertex v => GeoTransform v -> Pixel v -> Vertex v
-backwardGT gt p = v0 + (unPx p) *! m
+gtBackward :: IsVertex v Double => GeoTransform v -> Pixel v -> v Double
+gtBackward gt p = v0 + (unPx p) *! m
   where m  = gtMatrix gt
         v0 = gtOrigin gt
 
 data GeoReference v where
-  GeoReference :: forall v. IsVertex v =>
+  GeoReference :: forall v. IsVertex v Double =>
       { grTransform :: GeoTransform v
       , grSize      :: Size v
       , grSrs       :: SpatialReference
@@ -186,25 +214,29 @@ deriving instance Eq (GeoReference v)
 deriving instance Show (GeoReference v)
 deriving instance Typeable GeoReference
 
-grScalarSize :: IsVertex v => GeoReference v -> Int
+grScalarSize :: IsVertex v Double => GeoReference v -> Int
 grScalarSize = scalarSize . grSize
 
 
-vertexOffset :: IsVertex v => GeoReference v -> Vertex v -> Int
-vertexOffset gr =  linOffset (grSize gr) . forward gr
-{-# SPECIALIZE INLINE vertexOffset :: GeoReference V2 -> Vertex V2 -> Int #-}
+vertexOffset :: (HasOffset v t, IsVertex v Double)
+  => GeoReference v -> v Double -> Maybe (Offset t)
+vertexOffset gr =  toOffset (grSize gr) . grForward gr
+{-# SPECIALIZE INLINE
+      vertexOffset :: GeoReference V2 -> V2 Double -> Maybe (Offset RowMajor)
+      #-}
 
-forward :: IsVertex v => GeoReference v -> Vertex v -> Pixel v
-forward gr = forwardGT (grTransform gr)
-{-# INLINE forward #-}
+grForward :: IsVertex v Double => GeoReference v -> v Double -> Pixel v
+grForward gr = gtForward (grTransform gr)
+{-# INLINE grForward #-}
 
-backward :: IsVertex v => GeoReference v -> Pixel v -> Vertex v
-backward gr = backwardGT (grTransform gr)
-{-# INLINE backward #-}
+grBackward :: IsVertex v Double => GeoReference v -> Pixel v -> v Double
+grBackward gr = gtBackward (grTransform gr)
+{-# INLINE grBackward #-}
 
 
 mkGeoReference ::
-  (IsVertex v, R2 v, Eq (v Bool)) =>
+  ( IsVertex v Double, R2 v
+  , Eq (v Bool), Fractional (v Double)) =>
   Extent v -> Size v -> SpatialReference -> Either String (GeoReference v)
 mkGeoReference e s srs = fmap (\gt -> GeoReference gt s srs)
                               (northUpGeoTransform e s)
