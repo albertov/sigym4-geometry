00f7c6b68a242e0aff34a07ae944f6bd68379455
Alberto Valverde González <alberto@toscat.net>
2014-06-23 14:49:27 +0200

    updated to latest hspec

--- tests/Sigym4/Geometry/BinarySpec.hs
+++ tests/Sigym4/Geometry/BinarySpec.hs
@@ -16,45 +16,45 @@ spec :: Spec
 spec = do
   describe "Sigym4.Geometry.Binary" $ do
     describe "2D Point" $ do
-      it "deserializes the same thing it serializes" $ property $
+      prop "deserializes the same thing it serializes" $
         (encodeDecodeIsId :: (ByteOrder, Geometry Point V2) -> Bool)
     describe "3D Point" $ do
-      it "deserializes the same thing it serializes" $ property $
+      prop "deserializes the same thing it serializes" $
         (encodeDecodeIsId :: (ByteOrder, Geometry Point V3) -> Bool)
 
     describe "2D MultiPoint" $ do
-      it "deserializes the same thing it serializes" $ property $
+      prop "deserializes the same thing it serializes" $
         (encodeDecodeIsId :: (ByteOrder, Geometry MultiPoint V2) -> Bool)
     describe "3D MultiPoint" $ do
-      it "deserializes the same thing it serializes" $ property $
+      prop "deserializes the same thing it serializes" $
         (encodeDecodeIsId :: (ByteOrder, Geometry MultiPoint V3) -> Bool)
 
     describe "2D LineString" $ do
-      it "deserializes the same thing it serializes" $ property $
+      prop "deserializes the same thing it serializes" $
         (encodeDecodeIsId :: (ByteOrder, Geometry LineString V2) -> Bool)
     describe "3D LineString" $ do
-      it "deserializes the same thing it serializes" $ property $
+      prop "deserializes the same thing it serializes" $
         (encodeDecodeIsId :: (ByteOrder, Geometry LineString V3) -> Bool)
 
     describe "2D MultiLineString" $ do
-      it "deserializes the same thing it serializes" $ property $
+      prop "deserializes the same thing it serializes" $
         (encodeDecodeIsId :: (ByteOrder, Geometry MultiLineString V2) -> Bool)
     describe "3D MultiLineString" $ do
-      it "deserializes the same thing it serializes" $ property $
+      prop "deserializes the same thing it serializes" $
         (encodeDecodeIsId :: (ByteOrder, Geometry MultiLineString V3) -> Bool)
 
     describe "2D Polygon" $ do
-      it "deserializes the same thing it serializes" $ property $
+      prop "deserializes the same thing it serializes" $
         (encodeDecodeIsId :: (ByteOrder, Geometry Polygon V2) -> Bool)
     describe "3D Polygon" $ do
-      it "deserializes the same thing it serializes" $ property $
+      prop "deserializes the same thing it serializes" $
         (encodeDecodeIsId :: (ByteOrder, Geometry Polygon V3) -> Bool)
 
     describe "2D MultiPolygon" $ do
-      it "deserializes the same thing it serializes" $ property $
+      prop "deserializes the same thing it serializes" $
         (encodeDecodeIsId :: (ByteOrder, Geometry MultiPolygon V2) -> Bool)
     describe "3D MultiPolygon" $ do
-      it "deserializes the same thing it serializes" $ property $
+      prop "deserializes the same thing it serializes" $
         (encodeDecodeIsId :: (ByteOrder, Geometry MultiPolygon V3) -> Bool)
 
 encodeDecodeIsId :: (Decodable t, IsVertex v Double)
